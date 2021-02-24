b97c12bbbbd9ab12fc7cfe9229893f3167f430d8
SankhasubhraNandi11 <72122318+SankhasubhraNandi11@users.noreply.github.com>
2021-02-24 11:52:47 +0530

    Hi im sankhasubhra nandi

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Navbar from './Components/HomeNavbar/Navbar';
+import Resources from './Pages/Resources';
+import Schemes from './Pages/Schemes';
+import Farmanalysis from './Pages/Farmanalysis/Farmanalysis';
+import Dashboard from './Pages/Dashboard/Dashboard';
+import Store from './Pages/Store/Store';
+import Alerts from './Pages/Alerts/Alerts';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Dashboard} />
+          <Route path='/Farmanalysis' component={Farmanalysis} />
+          <Route path='/alerts' component={Alerts} />
+          <Route path='/store' component={Store} />
+          <Route path='/resources' component={Resources} />
+          <Route path='/schemes' component={Schemes} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
